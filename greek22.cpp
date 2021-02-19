82934b18564244d7bd03b54206db7c07152ba04d
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-02-19 17:08:35 +0530

    Add files via upload

---
+++ greek22.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+class Solution{
+
+    struct QueueNode
+    {
+        int i, j, distance;
+    };
+
+
+    private:
+    bool isValid(int i, int j, int M, int N)
+    {
+        if ((i < 0 || i > M - 1) || (j < 0 || j > N - 1))
+            return false;
+
+        return true;
+    }
+
+    bool isSafe(int i, int j, vector<vector<char> >& matrix, vector<vector<int> >& result)
+    {
+        if (matrix[i][j] != 'O' || result[i][j] != -1)
+            return false;
+
+        return true;
+    }
+
+    public:
+    vector<vector<int> > findDistance(vector<vector<char> >& matrix, int M, int N)
+    {
+        vector<vector<int> > result(M);
+        queue<QueueNode> q;
+
+        for (int i = 0; i < M; i++)
+        {
+            result[i].resize(N, -1);
+            for (int j = 0; j < N; j++)
+            {
+                if (matrix[i][j] == 'B')
+                {
+                    QueueNode node = {i, j, 0};
+                    q.push(node);
+                    result[i][j] = 0;
+                }
+            }
+        }
+
+        while (!q.empty())
+        {
+            QueueNode curr = q.front();
+            int x = curr.i, y = curr.j, dist = curr.distance;
+
+            int row[] = { -1, 0, 1, 0};
+            int col[] = { 0, 1, 0, -1 };
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (isValid(x + row[i], y + col[i], M, N)
+                    &&
+                    isSafe(x + row[i], y + col[i], matrix, result)
+                    )
+                {
+                    result[x + row[i]][y + col[i]] = dist + 1;
+
+                    QueueNode node = {x + row[i], y + col[i], dist + 1};
+                    q.push(node);
+                }
+            }
+
+            q.pop();
+        }
+        return result;
+    }
+};
