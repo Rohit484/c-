c414b2a4cf7a7dd7df574e5a6956d882e362db15
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-04-15 00:06:51 +0530

    Add files via upload

---
+++ sololearn4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    //your code goes here
+    for (int distance =40; distance <=120; distance +=40)
+    {
+       cout <<" "<< distance << endl  ;
+    }
+    return 0;
+}
