eb2e06fa83ce1e9ab7818d97cdab029340763a4f
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-04-27 21:17:09 +0530

    Add files via upload

---
+++ practice9.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    do{
+
+      cout<<n<<endl;
+   if(n%5==0)
+   {
+     cout<<"Beep"<<endl;
+       }
+      n--;
+       } while(n>0);
+
+
+
+
+   //your code goes here
+
+
+    return 0;
+}
