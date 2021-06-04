4749aec09c2be7471fcf0718ada254bab8b56083
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-06-04 20:09:19 +0530

    Add files via upload

---
+++ practice4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#define PERIOD 10
+#define PRINCIPAL 5000.00
+main()
+{
+    int year;
+    float amount ,value,inrate;
+    amount=PRINCIPAL;
+    inrate=0.11;
+    year=0;
+    while(year<=PERIOD)
+    {
+        printf("%2d   %8.2f\n",year,  amount);
+        value=amount+inrate*amount;
+        year=year+1;
+        amount=value;
+    }
+}
