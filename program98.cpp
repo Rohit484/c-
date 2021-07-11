67f98aad1138e8041471110314037c87be6b7821
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-07-11 20:43:12 +0530

    Add files via upload

---
+++ program98.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class Solution
+{
+    int atoi(string str)
+{
+    int num = 0;
+    bool isNegative = false;
+    int i = 0;
+
+    if(str[i] == '-')
+    {
+        isNegative = true;
+        i++;
+    }
+
+    for(; i < str.size(); i++)
+    {
+        if(isdigit(str[i]))
+        {
+            num = num * 10 + (str[i] - '0');
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    return (isNegative == true) ? -num : num;
+}
+};
